6dac75e9097a81bf4d9713b06e89daa190b281c0
harsha-mangena <harsha.mangena@gmail.com>
2022-10-26 11:03:53 +0530

    added .gitignore

--- foodordering/tests.py
+++ foodordering/tests.py
@@ -4,10 +4,14 @@ from rest_framework.test import APIClient
 
 from .models import Item, Store, User
 
+
+'''
+Test for User Registration
+'''
 @pytest.mark.django_db
 def test_for_user_registratin():
     user = User.objects.create_user('test', 'test@test.com', 'test')
-    store = Store.objects.create(user=user)
-    item = Item.objects.create(store=store)
-    assert item.store == store
+    assert user.username == 'test' 
+
+@pytest.mark.django_db
 
